4b3ae251395e79e99c7c29c80f8858f5cc9f5b09
jmaes12345 <jamesbrundell@hotmail.co.uk>
2021-01-30 18:12:14 +0000

    Bumped version of micronaut as issue with swagger docs was caused by a bug on their end - https://github.com/micronaut-projects/micronaut-core/issues/4753

--- app/backend/src/main/java/com/encountercalculator/Application.java
+++ app/backend/src/main/java/com/encountercalculator/Application.java
@@ -3,13 +3,17 @@ package com.encountercalculator;
 import com.encountercalculator.utils.ApplicationVariables;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "Encounter Calculator Backend",
                 version = ApplicationVariables.applicationVersion,
-                description = "My API"
+                description = "Backend API",
+                license = @License(name = "GNU General Public License v3.0", url = "https://www.gnu.org/licenses/gpl-3.0.en.html"),
+                contact = @Contact(name = "jmaes12345", url = "https://github.com/jmaes12345/EncounterCalculator")
         )
 )
 public class Application {
